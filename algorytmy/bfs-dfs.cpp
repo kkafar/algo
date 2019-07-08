63a8d7db87b0f2e26f41bfd3058258a65d3f7a36
kkafar <50801299+kkafar@users.noreply.github.com>
2019-07-08 12:08:54 +0200

    Usunięcie zbędnej zawartości
    Algorytm był przeniesiony z mojego ideone, było to roziwiązanie zadania SPOJ: Searching the graph. Posotawiam tylko kod dla BFS i DFS

--- algorytmy/bfs-dfs.cpp
+++ algorytmy/bfs-dfs.cpp
@@ -3,11 +3,7 @@
 #include <queue>
  
 using namespace std;
- 
-struct Pair
-{
-	int vertex, query_type;
-};
+
  
 void dfs(vector<int>G[], bool visited[], int current_vertex)
 {
@@ -50,67 +46,3 @@ void reset_visited_array(bool visited[], int numb_of_vertices)
 	for (int i = 1; i <= numb_of_vertices; ++i)
 		visited[i] = false;
 }
- 
-int main()
-{
-	int numb_of_graphs, numb_of_vertices, numb_of_adj, current_vertex, adjacent_vertex;
-	Pair query;
- 
-	ios_base::sync_with_stdio(false);
- 
-	cin >> numb_of_graphs;
- 
-	for (int i = 0; i < numb_of_graphs; ++i)
-	{
-		cin >> numb_of_vertices;
-		vector<int> * graph = new vector<int>[numb_of_vertices + 1];
-		bool * visited = new bool[numb_of_vertices + 1];
- 
-		for (int j = 1; j <= numb_of_vertices; ++j)
-		{
-			visited[j] = false;
-			cin >> current_vertex >> numb_of_adj;
- 
-			if (numb_of_adj != 0)
-			{
-				for (int k = 0; k < numb_of_adj; ++k)
-				{
-					cin >> adjacent_vertex;
-					graph[j].push_back(adjacent_vertex);
-				}
-			}
-		}
- 
-		cout << "graph " << i + 1 << endl;
- 
-		while (true)
-		{
-			cin >> query.vertex >> query.query_type;
- 
-			if (query.vertex == 0 && query.query_type == 0)
-				break;
- 
-			else if (query.query_type == 0)
-			{
-				dfs(graph, visited, query.vertex);
-				cout << endl;
-			}
- 
-			else
-			{
-				queue<int> vertices_to_process;
-				bfs(graph, visited, query.vertex, &vertices_to_process);
-				cout << endl;
-			}
- 
-			reset_visited_array(visited, numb_of_vertices);
-		}
- 
-		delete[] graph;
-		delete[] visited;
-	}
- 
-	return 0;
-}
- 
- 
