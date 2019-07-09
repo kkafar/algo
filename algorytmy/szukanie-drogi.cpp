f5341353c27ca669d1d6c49cbab6ba68da780b82
kkafar <50801299+kkafar@users.noreply.github.com>
2019-07-09 20:44:14 +0200

    Dodanie tablicy odwiedzin
    Dodano tablicę odwiedzin, algorytm nie odwiedza już teraz odwiedzonych wierzchołków - pomimo tego ciągle się wysypuje. 
    Naniesiono komentarze. Trzeba jeszcze tylko znaleźć błąd (najprawdopodobniej logiczny).

--- algorytmy/szukanie-drogi.cpp
+++ algorytmy/szukanie-drogi.cpp
@@ -1,14 +1,197 @@
-#include <iostream>
+/*
+	Moja implementacja podstawowej wersji algorytmu Dijkstry do wyszukiwania 
+	najkrótszej ścieżki w grafach nieskierowanych. 
+	Zgodnie z tym co czytałem, najlepiej jest rozpoczynać przeglądanie sąsiadów od najbliższego (algorytmem zachłannym), 
+	ale nie jest to konieczne. 
+*/
+
+#include <iostream> // do wyświetlania tablic i rezultatów
 #include <vector>
 #include <queue>
 #include <stack>
-
-// w rozwianiu zdecydowalem sie na zastosowanie cos ala macieerzy sasiedztwa do przechowywania wag krawedzi
-
-#define INF 10000
-
-
+#include <string>	// do wyświetlania tablic
 using namespace std;
+
+#define INF 100000
+
+void wyswietl_macierz(int ** macierz, int rozmiar);
+
+void wyswietl_graf(vector<int> * graf, int l_wierzcholkow);
+
+void wyswietl_tablice(int T[], int rozmiar, const string &s);
+
+void error(const string&);
+
+void dij(vector<int> * G, int w_s, int ** W, bool * V, int * P, int * D, queue<int> * q, int l_w)
+{
+	// zmienna pomocnicza przechowująca drogę z rozważanego wierzchołka do rozważanego sąsiada (aby nie liczyć jej dwukrotnie)
+	int d;
+	// umieszczamy w kolejce wierzchołek startowy algorytmu
+	q->push(w_s);
+	// Bo droga z w_startowy do samego siebie wynosi 0
+	D[w_s] = 0;
+	P[w_s] = w_s;
+	// dopóki kolejka nie jest pusta, czyli dopóki nie skończą się wierzchołki grafu
+	while (!q->empty())
+	{
+		// pobieramy pierwszy wierzchołek z kolejki
+		w_s = q->front();
+		// zabieramy rozważany wierzchołek z kolejki
+		q->pop();
+		// rozważany wierzchołek oznaczamy jako odwiedzony
+		V[w_s] = true;
+
+		// przeglądamy wszystkich jego nieodwiedzonych sąsiadów
+		for (auto i = G[w_s].begin(); i != G[w_s].end(); ++i)
+		{
+			if (!V[*i])
+			{
+				// liczymy drogę z obecnie rozważanego wierzchołka do obecnie rozważanego sąsiada
+				d = D[w_s] + W[w_s][*i];
+				// jeżeli nowo policzona droga jest krótsza od tej dotychczas znanej to podmieniamy
+				// oraz zmieniamy wierzchołek z którego przyszliśmy 
+				if (d < D[*i])
+				{
+					D[*i] = d;
+					// oraz zmieniamy wierzchołek z którego przyszliśmy 
+					P[*i] = w_s;
+				}
+			}
+			// dodajemy sąsiada do kolejki, aby go później rozważyć. 
+			q->push(*i);
+		}
+	}
+	/* Po wykonianiu się tej funkcji, wszystkie tablice powinny zawierać prawidłowe rezultaty. */
+}
+
+void sciezka(int w_s, int w_k, int P[])
+{
+	// Jako że ścieżkę będziemy odtwarzać od końca, to kolejno uzyskiwane wierzchołki najwygodniej wrzucać jest na stos. 
+	stack<int> s;
+	// umieszczamy na samym spodzie stosu wierzchołek koncowy
+	s.push(w_k);
+
+	// dopóki wierzchołek końcowy nie stanie się wierzchołkiem startowym
+	while (w_k != w_s)
+	{
+		// przypisz do w_koncowy indeks jego poprzednika
+		w_k = P[w_k];
+		// umiesc w_koncowy na stosie
+		s.push(w_k);
+	}
+	// wyświetlanie stosu
+	while (!s.empty())
+	{
+		cout << s.top() << " ";
+		s.pop();
+	}
+	cout << "\n";
+}
+
+int main()
+{
+	/* WEJŚCIE I ZMIENNE DO JEGO OSŁUGI */
+	/// =======================================
+	int l_wierzcholkow, l_krawedzi, w_startowy, w_koncowy, l_testow, k_waga;
+
+	// kolejka potrzebna do algorytmu
+	queue<int> q;
+
+	cout << "Podaj liczbe wierzcholkow i liczbe krawedzi: ";
+	cin >> l_wierzcholkow >> l_krawedzi;
+	/// =======================================
+
+	/* REZERWACJA PAMIĘCI */
+	/// =======================================
+	// reprezentacja grafu w postaci lisy sąsiedztwa
+	vector<int> * G = new vector<int>[l_wierzcholkow];
+
+	// macierz przechowująca wagi poszczególnych krawędzi
+	// W[i][j] - to waga krawędzi e(w_i, w_j) (w_i to wierzchołek o indeksie i)
+	int ** W = new  int *[l_wierzcholkow];
+	for (int i = 0; i < l_wierzcholkow; ++i)
+	{
+		// tworzenie tablic 1D składających się na całą macierz
+		W[i] = new int[l_wierzcholkow];
+
+		// inicjowanie zerami całej macierzy
+		for (int j = 0; j < l_wierzcholkow; ++j)
+			W[i][j] = 0;
+	}
+
+	// tablica wierzcholkow odwiedzonych
+	bool * V = new bool[l_wierzcholkow];
+
+	// P[i] - zawiera indeks wierzcholka z którego doszliśmy do w_i
+	int * P = new int[l_wierzcholkow];
+
+	// D[i] - zawiera najkrótszą obecnie znaną drogę z w_startowy do w_i
+	int * D = new int[l_wierzcholkow];
+
+	// petla inicjująca tablice V[], P[], D[]
+	for (int i = 0; i < l_wierzcholkow; ++i)
+	{
+		V[i] = false;
+		// bo jeszcze nie wiemy z jakiego wierzcholka przyszlismy
+		P[i] = -1; 
+		// INF = 100000, naiwnie zakładamy że większa wartość na wejściu nie wystąpi
+		D[i] = INF;
+	}
+	/// =======================================
+
+	/* WEJŚCIE C.D */
+	/// =======================================
+	cout << "Podaj kolejne krawedzie wraz z ich wagami:\n";
+
+	for (int i = 0; i < l_krawedzi; ++i)
+	{
+		cin >> w_startowy >> w_koncowy >> k_waga;
+		// uzupełniamy graf 
+		G[w_startowy].push_back(w_koncowy);
+		// uzupełniamy macierz z wagami poszczególnych krawędzi. 
+		W[w_startowy][w_koncowy] = k_waga;
+	}
+
+	cout << "Podaj indeks wierzcholka poczatkowego oraz wierzcholka koncowego: ";
+	cin >> w_startowy >> w_koncowy;
+	/// =======================================
+
+	error("Przed algo");
+
+	/* ALGORYTM WYSZUKUJĄCY NAJKRÓTSZĄ ŚCIEŻKĘ (DIJKSTRA) */
+	/// =======================================
+	dij(G, w_startowy, W, V, P, D, &q, l_wierzcholkow);
+	/// =======================================
+
+	error("po algo");
+
+	/* WYŚWIETLANIE ZAWARTOSCI TABLIC, GRAFU I MACIERZY 
+	   W CELU DEBBUGOWANIA */
+	/// =======================================
+	wyswietl_macierz(W, l_wierzcholkow);
+
+	wyswietl_graf(G, l_wierzcholkow);
+
+	wyswietl_tablice(P, l_wierzcholkow, "POPRZEDNIKOW");
+
+	wyswietl_tablice(D, l_wierzcholkow, "DROG");
+	/// =======================================
+
+	/* WYŚWIETLENIE REZULTATU, korzystając z funkcji sciezka(...) */ 
+	/// =======================================
+	cout << "Najkrotsza droga: " << D[w_koncowy] << " | poprzez wierzcholki: ";
+	sciezka(w_startowy, w_koncowy, P);
+	/// =======================================
+
+	delete[] G;
+	for (int i = 0; i < l_wierzcholkow; ++i)
+		delete[] W[i];
+
+	delete[] V;
+	delete[] P;
+	delete[] D;
+	return 0; 
+}
 
 void wyswietl_macierz(int ** macierz, int rozmiar)
 {
@@ -38,140 +221,16 @@ void wyswietl_graf(vector<int> * graf, int l_wierzcholkow)
 }
 
 // wyswietl tablice
-void wyswietl_tablice(int T[], int rozmiar)
+void wyswietl_tablice(int T[], int rozmiar, const string& s)
 {
-	cout << "TABLICA\n";
+	cout << "TABLICA " << s << "\n";
 	for (int i = 0; i < rozmiar; ++i)
 		cout << T[i] << " ";
-	
+
 	cout << "\n";
 }
 
-//  (graf do przejrzenia; wierzcholek startowy; kolejka, tablica odwiedzin, tablica drog, tablica poprzednikow
-void znajdz_droge(vector<int> * graf, int w, queue<int> * q, int d[], int ** waga_krawedzi, int sciezka[])
+void error(const string&s)
 {
-	q->push(w);
-	d[w] = 0;
-	sciezka[w] = w; 
-
-	while (!q->empty())
-	{
-		// pobieram pierwszy w kolejce element
-		w = q->front();
-		q->pop();
-		
-		for (auto i = graf[w].begin(); i != graf[w].end(); ++i)
-		{
-			// jezeli nie patrze na ten z ktorego przyjechalem
-			if ((*i) != w)
-			{
-				// jezeli droga z aktualnie przegladanego wierzcholka w, do jego sasiada jest krotsza niz aktualna najkrotsza droga do sasiada, to podmien
-				if (d[w] + waga_krawedzi[w][*i] < d[*i])
-				{
-					d[*i] = d[w] + waga_krawedzi[w][*i];
-					// skoro podmieniamy to do sasiada (*i) przychodzimy z wierzcholka w
-					sciezka[*i] = w;
-				}
-
-				// dodaj sasiada do kolejki
-				q->push(*i);
-			}
-
-		}
-	}
-}
-
-void wyswietl_sciezke(int w_startowy, int w_koncowy, int sciezka[])
-{
-	cout << "SZIEZKA: \n";
-	stack<int> s;
-	int w = w_koncowy; 
-	s.push(w);
-	while (w != w_startowy)
-	{
-		w = sciezka[w];
-		s.push(w);
-	}
-	
-	while (!s.empty())
-	{
-		cout << s.top() << " ";
-		s.pop();
-	}
-	cout << "\n";
-}
-
-int main()
-{
-	queue<int> q;
-	int l_wierzcholkow, l_krawedzi, w_startowy, w_koncowy, waga;
-
-	cout << "podaj liczbe wierzcholkow oraz liczbe krawedzi grafu: ";
-	cin >> l_wierzcholkow >> l_krawedzi;
-
-	vector<int> * graf = new vector<int>[l_wierzcholkow];
-
-	// dynamiczne tworzenie tablicy 2D do przechowywania wag krawedzi;
-	// =================================== 
-	int ** waga_krawedzi = new int*[l_wierzcholkow];
-
-	for (int i = 0; i < l_wierzcholkow; ++i)
-	{
-		// tworzenie wierszy
-		waga_krawedzi[i] = new int[l_wierzcholkow];
-
-		// tablice uzupelniamy wartosciami 0
-		for (int j = 0; j < l_wierzcholkow; ++j)
-			waga_krawedzi[i][j] = 0;
-	}
-	// ===================================
-
-	// droga_do[i] - najkrotsza dotychczas znana droga z wierzcholka startowego do wiercholka w_i
-	int * droga_do = new int[l_wierzcholkow];
-
-	for (int i = 0; i < l_wierzcholkow; ++i)
-		droga_do[i] = INF;
-
-
-	// s[i] - przechowuje index wierzcholka z którego przechodzimy na w_i
-	int * sciezka = new int[l_wierzcholkow];
-
-	for (int i = 0; i < l_wierzcholkow; ++i)
-		sciezka[i] = -1; 
-
-	// wczytywanie krawedzi grafu wraz z ich wagami
-	cout << "podaj kolejne krawedzie grafu wraz z wagami krawedzi:\n";
-	for (int i = 0; i < l_krawedzi; ++i)
-	{
-		cin >> w_startowy >> w_koncowy >> waga;
-		graf[w_startowy].push_back(w_koncowy);
-		waga_krawedzi[w_startowy][w_koncowy] = waga;
-	}
-	cout << "\npodaj wierzcholek startowy: ";
-	cin >> w_startowy;
-	cout << "podaj wierzcholek koncowy: ";
-	cin >> w_koncowy;
-	// wyswietlanie wszsytkiego do debugowania
-	wyswietl_macierz(waga_krawedzi, l_wierzcholkow);
-
-	wyswietl_graf(graf, l_wierzcholkow);
-
-	znajdz_droge(graf, w_startowy, &q, droga_do, waga_krawedzi, sciezka);
-
-	wyswietl_tablice(droga_do, l_wierzcholkow);
-
-	cout << "DROGA: " << droga_do[w_koncowy] << "\n";
-
-	//wyswietl_tablice(sciezka, l_wierzcholkow);
-
-	wyswietl_sciezke(w_startowy, w_koncowy, sciezka);
-
-	// zwalnianie zarezerwowanej pamieci operacyjnej
-	for (int i = 0; i < l_wierzcholkow; ++i)
-		delete[] waga_krawedzi[i];
-
-	delete[] graf;
-	delete[] droga_do;
-	delete[] sciezka;
-	return 0;
+	cout << s << "\n";
 }
