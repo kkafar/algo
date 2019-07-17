00c744a8dffd1b457b81da708b7f4e16e6e1ed45
kkafar <50801299+kkafar@users.noreply.github.com>
2019-07-17 13:36:51 +0200

    Dodanie komentarza  do algorytmu

--- algorytmy/sortowanie_przez_wstawianie.cpp
+++ algorytmy/sortowanie_przez_wstawianie.cpp
@@ -2,30 +2,44 @@
 #include <string>
 using namespace std;
  
- 
+/* ALGORYTM SORTOWANIA PRZEZ WSTAWIANIE */
+/// ===================================
 template<class type>
 void sortowanie_przez_wstawianie(type T[], int rozmiar_tablicy)
 {
 	type ustawiana_liczba;
 	int index_liczby_po_lewej;
+	// zaczynamy iterowanie od i == 1, bo index_liczby_po_lewe = i-1 ==> inaczej nie miałoby
+	// to sensu
 	for (int i = 1; i < rozmiar_tablicy; ++i)
 	{
+		// zapamiętujemy rozważaną liczbę
 		ustawiana_liczba = T[i];
 		index_liczby_po_lewej = i - 1; 
+		// czyli do poki nie dojedziemy do pierwszego elementu tablicy
 		while (index_liczby_po_lewej >= 0)
 		{
+			// jeżeli liczba po naszej lewej jest od nas większa
 			if (ustawiana_liczba < T[index_liczby_po_lewej])
 			{
+				// to przesuwamy tą liczbę o raz w prawo
 				T[index_liczby_po_lewej + 1] = T[index_liczby_po_lewej];
+				// i idziemy dalej
 				--index_liczby_po_lewej;
 			}
  
+			// jeżeli napotkaliśmy po naszej lewej stronie liczbę mniejszą bądź równą to wychodzimy z while'a
 			else
 				break;
 		}
+		// skoro tu jesteśmy to albo przejechaliśmy aż do pierwszego elementu tablicy
+		// albo po naszej lewej stronie stoi liczba mniejsza bądź równa od ustawiana_liczba
+		// skoro tak to tam skąd wcześniej przesunęliśmy wartość o raz w prawo wpisujemy naszą
+		// ustawianą liczbę.
 		T[index_liczby_po_lewej + 1] = ustawiana_liczba;
 	}
 }
+/// ===================================
  
 template<class type>
 void wyswietl_tablice(type T[], int rozmiar_tablicy)
