a05b36fe426c2088d86c96b320166ba34eedd115
kkafar <50801299+kkafar@users.noreply.github.com>
2019-07-21 16:44:30 +0200

    Zmiana algorytmu sortującego
    Ze sortowania przez zliczanie na sortowanie przez scalanie

--- algorytmy/porownanie-algorytmow-wyszukujacych.cpp
+++ algorytmy/porownanie-algorytmow-wyszukujacych.cpp
@@ -161,45 +161,101 @@ int wyszukiwanie_liniowe(int T[], int l_elementow, int klucz)
 }
 /// ===================================
 
-/* ALGORYTM SORTOWANIA PRZEZ WSTAWIANIE */
-/// ===================================
-// zdecydowałem się na niego, żeby dodać komentarze na githuba :D
-template<class type>
-void sortowanie_przez_wstawianie(type T[], int rozmiar_tablicy)
+/* ALGORYTM REALIZUJĄCY SCALANIE DWÓCH ZBIORÓW */
+/// ================================================
+// operujemy na podzbiorach sortowanej tablicy, zatem funkcja jako argumenty przyjmuje 
+// indeksy granic scalanych przedziałów: 
+// i_lewy_początek - indeks pierwszego elementu lewego przedziału (tego o indeksach mniejszych)
+// i_prawy_poczatek  - indeks pierwszego elemntu prawego przedziału (tego o indeksach większych)
+// i_prawy_koniec - indeks ostatniego elementu prawego przedziału
+// i_lewy_koniec - nie pobieramy, bo to przecież (i_prawy_poczatek - 1);
+void merge(int T[], int i_lewy_poczatek, int i_prawy_poczatek, int i_prawy_koniec)
 {
-	type ustawiana_liczba;
-	int index_liczby_po_lewej;
-	// zaczynamy iterowanie od i == 1, bo index_liczby_po_lewe = i-1 ==> inaczej nie miałoby
-	// to sensu
-	for (int i = 1; i < rozmiar_tablicy; ++i)
+	// tworzymy sobie kopie pobranych indeksów, bo będziemy potrzebować zmieniać ich wartości jednocześnie
+	// pamiętając ich wartości początkowe
+	// i1 - będziemy się za jego pomocą poruszać po lewej części przedziału
+	// i2 - będziemy sie za jego pomocą poruszać po prawej części przedziału
+	int i1 = i_lewy_poczatek, i2 = i_prawy_poczatek;
+	// iterator służący do śledzenia miejsca wpisywania do tablicy pomocniczej P[]
+	register int i = 0;
+
+	// tworzymy tablicę pomocniczą, przechowującą tymczasowo scalony zbiór (na końcu przepiszemy jej zawartość do 
+	// wejściowej tablicy)
+	int * P = new int[i_prawy_koniec - i_lewy_poczatek + 1];
+
+	// dopóki iteratory nie wyjdą poza przedziały na które mają wskazywać
+	while (i1 < i_prawy_poczatek && i2 <= i_prawy_koniec)
 	{
-		// zapamiętujemy rozważaną liczbę
-		ustawiana_liczba = T[i];
-		index_liczby_po_lewej = i - 1; 
-		// czyli do poki nie dojedziemy do pierwszego elementu tablicy
-		while (index_liczby_po_lewej >= 0)
+		// jeżeli pierwszy element prawej części talicy jest mniejszy, to jego wpisujemy do tablicy pomocniczej P[]
+		if (T[i1] > T[i2])
 		{
-			// jeżeli liczba po naszej lewej jest od nas większa
-			if (ustawiana_liczba < T[index_liczby_po_lewej])
-			{
-				// to przesuwamy tą liczbę o raz w prawo
-				T[index_liczby_po_lewej + 1] = T[index_liczby_po_lewej];
-				// i idziemy dalej
-				--index_liczby_po_lewej;
-			}
- 
-			// jeżeli napotkaliśmy po naszej lewej stronie liczbę mniejszą bądź równą to wychodzimy z while'a
-			else
-				break;
+			// przypisujemy, a następnie inkrementujemy i2;
+			P[i] = T[i2++];
 		}
-		// skoro tu jesteśmy to albo przejechaliśmy aż do pierwszego elementu tablicy
-		// albo po naszej lewej stronie stoi liczba mniejsza bądź równa od ustawiana_liczba
-		// skoro tak to tam skąd wcześniej przesunęliśmy wartość o raz w prawo wpisujemy naszą
-		// ustawianą liczbę.
-		T[index_liczby_po_lewej + 1] = ustawiana_liczba;
+		// w wypadku gdy wartości są równe, bądź to pierwsza liczba w lewej części tablicy jest mniejsza
+		else
+		{
+			P[i] = T[i1++];
+		}
+
+		// zwiększamy iterator i;
+		++i;
 	}
+
+	// skoro wyszliśmy już za tą pętlę, to znaczy, że któraś część tablicy się już wyczerpała, więc resztę z drugiej części
+	// przepisujemy do tablicy pomocniczej P[]
+	while (i1 < i_prawy_poczatek)
+	{
+		P[i++] = T[i1++];
+	}
+	while (i2 <= i_prawy_koniec)
+	{
+		P[i++] = T[i2++];
+	}
+
+	// na koniec pozostaje przepisać zawartość tablicy pomocniczej P[] do tablicy T[]
+	// tablice P[], uzupełnialiśmy od indeksu 0 (zawsze), więc trzeba przeliczyć indeksy. 
+	for (int j = i_lewy_poczatek; j <= i_prawy_koniec; ++j)
+		T[j] = P[j - i_lewy_poczatek];
+
+
+
+	delete[] P;
 }
-/// ===================================
+/// ================================================
+
+
+/* ALGORYTM SORTUJĄCY */ 
+/// ================================================
+// wykorzystujemy rekrurencję
+// jako argumenty przyjmujemy indeksy graniczne sortowanego zbioru: 
+// i_lewy_poczatek - indeks pierwszego elementu z lewej strony sortowanego zbioru
+// i_prawy_koniec - indeks ostatniego elementu w prawej strony sortowanego zbioru
+// przy pierwszym wywołaniu funkcji, powinniśmy podać granice zbioru który chcemy posortować
+// czyli najczęściej 0 i (l_elementow - 1)
+void merge_sort(int T[], int i_lewy_poczatek, int i_prawy_koniec)
+{
+	// zmienna w której będziemy przechowywać indeks pierwszego elementu w prawym podzbiorze
+	// jako że korzystamy z rekurencji to będziemy sobie ją na bieżąco obliczać za każdym wywołaniem funkcji
+	// od razu ustawiamy się na środku rozpatrywanego zbioru (może w nawiasie powinno być jeszcze +1)
+	int i_prawy_poczatek = (i_lewy_poczatek + i_prawy_koniec + 1) / 2;
+
+	// dzielimy zbiór, aż dojedziemy do singletonów
+
+	// stoimy pośrodku zbioru [i_lewy_poczatek ; i_prawy_koniec]
+	// jeżeli zbiór po naszej lewej jest więcej niż 1 elementowy, to wywołujemy się na nim rekurencyjnie
+	// warunek: jeżeli (i_prawy_poczatek - 1 - i_lewy_poczatek > 0) <==> i_prawy_poczatek - i_lewy_poczatek > 1
+	if (i_prawy_poczatek - i_lewy_poczatek > 1)
+		merge_sort(T, i_lewy_poczatek, i_prawy_poczatek - 1);
+
+	// jeżeli zbiór po naszej prawej jest więcej niż 1 elementowy to wywpłujemy sie na nim rekurencyjnie
+	if (i_prawy_koniec - i_prawy_poczatek > 0)
+		merge_sort(T, i_prawy_poczatek, i_prawy_koniec);
+
+	merge(T, i_lewy_poczatek, i_prawy_poczatek, i_prawy_koniec);
+}
+/// ================================================
+
 
 
 void wyswietl_tablice(int T[], int l_elementow, const string& s)
@@ -260,7 +316,7 @@ int main()
 	/* SORTOWANIE */ 
 	/// ===================================
 	t1 = clock();
-	sortowanie_przez_wstawianie(T, l_elementow);
+	merge_sort(T, 0, l_elementow -1);
 	t2 = clock();
 	dt = (double)(t2 - t1) / CLOCKS_PER_SEC;
 	cout << "czas sortowania: " << dt << "\n";
