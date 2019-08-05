5bfa40c17a8ab2b8520f7cd47de77529d37a5f93
kkafar <50801299+kkafar@users.noreply.github.com>
2019-08-05 10:36:09 +0200

    Tworzenie kopca
    	Kopiec to takie drzewo, w którym rodzic jest zawsze większy bądź równy swoim dzieciom (warunek kopca)

---
+++ struktury-danych/tworzenie-kopca.cpp
@@ -0,0 +1,89 @@
+/* 
+	Tworzenie kopca. 
+	Kopiec to takie drzewo, w którym rodzic jest zawsze większy bądź równy swoim dzieciom (warunek kopca)
+
+*/ 
+
+#include <iostream>
+#include <time.h> 	// do generowania liczb losowych
+using namespace std;
+
+/* PROCEDURA WYŚWIETLAJĄCA DRZEWO */ 
+// =====================================
+template<class C> void printTree(C * D, int nodes)
+{
+	int lvl = 1, counter = 1; 
+	for (int i = 1; i <= nodes; ++i)
+	{
+		cout <<  " " << D[i];
+		if (counter == lvl)
+		{
+			counter = 0; 
+			lvl <<= 1; 
+			cout << "\n";
+		}
+		++counter;
+	}
+	cout << "\n";
+}
+// =====================================
+
+int main()
+{
+	// zmienna przechowująca liczbę węzłów w drzewie
+	// w - zmienna pomocnicza do rezlizowanai warunku kopca
+	// buffer - zmienna pomocnicza służąca do podmiany wartości w tablicy
+	int lWezlow, w, buffer;
+
+	// pobieramy liczbe węzłów
+	cin >> lWezlow;
+
+	// tworzymy tablicę którą będziemy reprezentować drzewo (indeksy 1...lWezlow)
+	double * D = new double[lWezlow + 1];
+	double * D2 = new double[lWezlow + 1];
+
+	// pobieramy kolejne węzły drzewa, od razu dbając o to, aby spełniony był warunek kopca. 
+	// Każdy element w momencie dodania jest "ostatnim liściem" ==> sprawdzamy czy kolejni rodzice są od nas więksi, 
+	// jeżeli tak, to zamieniamyu miejscami w tablicy odpowiednie dwa elementy. Jeśli nie, to znaczy że mamy kopiec
+	// jak się patrzy :) 
+	for (int i = 1; i <= lWezlow; ++i)
+	{
+		// pobieramy dane węzła i
+		cin >> D[i]; 
+		// od razu tworzymy kopie drzewa do wyswietlenia
+		D2[i] = D[i];
+
+		// realizujemy warunek kopca: 
+		// zmienna pomocnicza w, którą będziemy podróżować przez kolejne pokolenia rodziców wierzchołka i
+		w = i; 
+		// będziemy przyrównywać się do w/2, więc w ostatniej iteracji pętli możemy być najwyżej dla w == 2
+		while (w > 1)
+		{
+			// jeżeli rodzic jest mniejszy od swojego dziecka
+			if (D[w/2] < D[w])
+			{
+				// to podmieniamy ich wartościami. Rodzic staje się dzieckiem, a dziecko rodzicem
+				buffer = D[w]; 
+				D[w] = D[w/2]; 
+				D[w/2] = buffer;
+				w /= 2; 
+			}
+			// jeżeli if się nie wykonał, to znaczy, że dziecko jest na właściwym miejscu (spełniony jest warunek kopca)
+			// zatem możemy bezpiecznie przerwać działanie pętli 
+			else 
+				break;
+		}
+	}
+	// po wykonaniu tej pętli nasze drzewo powinno być kopcem. 
+
+	// wyświetlamy kopiec, i drzewo którego nie modyfikowalismy
+	cout << "Drzewo: \n";
+	printTree(D2, lWezlow);
+	cout << "\nKopiec: \n";
+	printTree(D, lWezlow);
+
+	// zwalniamy zarezerwowaną pamięć 
+	delete[] D; 
+	delete[] D2; 
+	return 0;
+}
