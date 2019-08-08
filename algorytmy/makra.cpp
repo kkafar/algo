8ff093c3c6a25f1886e4ff40d3bf48deff4efbe5
kkafar <50801299+kkafar@users.noreply.github.com>
2019-08-08 21:46:38 +0200

    Poprawki (dodanie podłóg)

--- algorytmy/makra.cpp
+++ algorytmy/makra.cpp
@@ -9,10 +9,10 @@ typedef long long LL;
 #define FOR(x, b, e) for(int x = b; x <= (e); ++x)
 #define FORD(x, b, e) for(int x = b; x >= (e); – –x)
 #define REP(x, n) for(int x = 0; x < (n); ++x)
-#define VAR(v, n) typeof(n) v = (n)
+#define VAR(v, n) __typeof(n) v = (n)
 #define ALL(c) (c).begin(), (c).end()
 #define SIZE(x) ((int)(x).size())
 #define FOREACH(i, c) for(VAR(i, (c).begin()); i != (c).end(); ++i)
-#define PB push back
+#define PB push_back
 #define ST first
 #define ND second
