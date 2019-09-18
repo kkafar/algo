b54b1abb005a220d525f1fae5a4cdc44a71b3631
kkafar <50801299+kkafar@users.noreply.github.com>
2019-09-18 13:24:26 +0200

    Dodanie drugiego algorytmu szybkiego potęgowania
    Pierwszy algorytm zapisany jest wprost z definicji szybkiego potęgowania (wykorzystuje wykładnik w postaci binarnej pośrednio)
    Drugi wykorzystuje wprost wykładnik w postaci binarnej.

--- algorytmy/szybkie-potegowanie-moje.cpp
+++ algorytmy/szybkie-potegowanie-moje.cpp
@@ -35,6 +35,39 @@ ll power(int a, int p)	// a - podstawa potęgi, p wykładnik.
 }
 /// =============================
 
+
+// ogólnie zasada działania pokażemy na przykładzie :
+/*
+	5^7: 
+	7 = (0111) = 2^2 + 2^1 + 2^0
+	
+	Zauważmy że 5^7 = 5^4 × 5^2 × 5^1
+	Natomiast 4 = 2^2, 2 = 2^1, 1 = 2^0
+	Zatem widzimy że musimy wymnożyć ze sobą takie potęgi liczby 5, których wykładniki 
+	są równe wagom niezerowych bitów liczby siedem.
+*/
+long long power2(int a, int p) 
+{
+	// result - zmienna przechowująca wynik, przypisujemy na starcie 1 bo gdy p == 0, pętla się nie wykona
+	// (a przecież chcemy zwrócić wtedy 1)
+	// M - a ^ (waga rozważanego bitu wykładnika)
+	long long result = 1, M = a;
+
+	// dopóki wykładnik nie jest zerowy
+	while (p)
+	{
+		// jeżeli najmłodszy bit wykładnika jest jedynką to dodajemy tą potęgę liczby a do jej rozkładu 
+		if (p & 1) result *= M; 
+
+		// a ^ (waga kolejnego bitu wykładnika)
+		M *= M; 
+		// przesuwamy bity wykładnika raz w prawo
+		p >>= 1;
+	}
+
+	return result;
+}
+
 int main()
 {
 	int a, p, z;
@@ -43,6 +76,7 @@ int main()
 	{
 		cin >> a >> p;
 		cout << power(a, p) << "\n";
+		cout << power2(a, p) << "\n";
 	}
 
 	return 0; 
