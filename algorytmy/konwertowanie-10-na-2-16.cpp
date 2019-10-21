698373c7bcb5ff2e05611c5ed64364b47c762df5
kkafar <50801299+kkafar@users.noreply.github.com>
2019-10-21 20:27:40 +0200

    Konwertowanie liczby z sys. 10. na dowolny z przedziału 2-16
    Zadanie robione przy okazji WDI, 21.10.19r.

---
+++ algorytmy/konwertowanie-10-na-2-16.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void error(string err_mssg)
+{
+    cout << err_mssg << "\n";
+}
+
+/* 
+    Funkcja konwertująca liczbę z systemu dziesiętnego na system o dowolnej podstawie
+    z przedziału [2, 16]. Zwraca liczbę jako string.
+    n - liczba w systemie dziesiętnym przeznaczona do konwertowania
+    sys_base - podstawa systemu liczbowego do którego konwertujemy liczbę n
+*/
+string convert(int n, int sys_base)
+{
+    // jako że liczbę będziemy mieć zapisaną od końca, to rozróżniamy liczbę 
+    // powstałą w wyniku algorytmu od zwracanego wyniku
+    string reversed_value, return_value;
+
+    // dopóki nie przetworzymy n do końca
+    while (n > 0)
+    {
+        if (n % sys_base < 10)
+        {
+            // +48 wynika z tablicy ascii 
+            reversed_value += (char)(n % sys_base + 48);
+            n /= sys_base;
+        }
+        else // czyli w przypadku gdy reszta wychodzi poza cyfry 0…9 => będziemy potrzebowali zapisać literę
+        {
+            // +55 tak samo wynika z tablicy ascii
+            reversed_value += (char)(n % sys_base + 55);
+            n /= sys_base;
+        }
+    }
+    // pętla odwracająca reversed_value
+    for (int i = reversed_value.length() - 1; i >= 0; --i)
+        return_value += reversed_value[i];
+
+    return return_value;
+}
+
+int main()
+{
+    int n; 
+
+    cin >> n;
+
+    for (int i = 2; i <= 16; ++i)
+    {
+        cout << i << ": " << convert(n, i) << "\n";
+    }
+    return 0;
+}
