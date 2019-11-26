365b0a237d26fb8da4327f13a63dcda8675a5abd
kkafar <50801299+kkafar@users.noreply.github.com>
2019-11-26 15:44:31 +0100

    Poprawiono szukanie najmniejszego podzbioru

--- zadania/WDI_Grzeszczuk/zestaw-7/zad3.cpp
+++ zadania/WDI_Grzeszczuk/zestaw-7/zad3.cpp
@@ -1,16 +1,11 @@
 /*
     ZESTAW 7
     ZADANIE 3
-    7. Dana jest tablica int t[N]. Proszę napisać funkcję, która znajdzie niepusty,
+7. Dana jest tablica int t[N]. Proszę napisać funkcję, która znajdzie niepusty,
 najmniejszy (w sensie liczebności) podzbiór elementów tablicy, dla którego suma
 elementów jest równa sumie indeksów tych elementów. Do funkcji należy przekazać
 tablicę, funkcja powinna zwrócić sumę elementów znalezionego podzbioru. Na przykład
 dla tablicy: [ 1, 7, 3, 5, 11, 2 ] rozwiązaniem jest liczba 10.
-
-Uwagi: 
-Nie jestem pewien (a raczej przychylam się do zdania że "nie") czy zawsze znajduje najmniejszy podzbiór
-Do tego jeżeli na indeksie 0 stoi liczba 0 to też nie powinno zadziałać prawidłowo (czyli nie zwróci 0) (ale nie
-sprawdzałem)
 */
 
 #include <iostream>
@@ -23,7 +18,7 @@ int MOC = 1000000; // załóżmy że inf
 int T[N] = {1, 7, 3, 5, 11, 2};
 
 
-int func(int t[N], int idx = 0, int sumaEl = 0, int sumaIdx = 0, int moc = 0)  // func(t, 0, 0, 0, 0)
+int func(int t[N], int idx = 0, int sumaEl = 0, int sumaIdx = 0, int moc = 0)  // func(t, 0, t[0], 0, 1)
 {
     if ((sumaEl == sumaIdx) && sumaIdx != 0)
     {
@@ -34,28 +29,32 @@ int func(int t[N], int idx = 0, int sumaEl = 0, int sumaIdx = 0, int moc = 0)  /
     {
         return -1;
     } 
+    
     // jeżeli mam moc mojego zbioru mniejszą niż aktualne minimum (MOC) to jest sens się w ogóle dalej wywoływać
-    if (moc < MOC)
-    {
-        // to mogę wziąć element, lub go nie wziąć 
-        // nie biorę elementu: to wywołuję się: dla tej samej tablicy, na kolejnym elemencie, z tą samą sumą elementów w zbiorze,
-        // z tą samą suma indeksów elementów w zbiorze, z tą samą mocą zbioru
-        // Jeżeli to wywołanie zwróci mi coś innego niż -1, to znaczy że dla tak podjętej decyzji udało się znaleźć zbiór spełniający warunki zadania
+    // to mogę wziąć element, lub go nie wziąć 
+    // nie biorę elementu: to wywołuję się: dla tej samej tablicy, na kolejnym elemencie, z tą samą sumą elementów w zbiorze,
+    // z tą samą suma indeksów elementów w zbiorze, z tą samą mocą zbioru
+    // Jeżeli to wywołanie zwróci mi coś innego niż -1, to znaczy że dla tak podjętej decyzji udało się znaleźć zbiór spełniający warunki zadania
+    if (moc < MOC) 
+    {       
         int niebiore = func(t, idx + 1, sumaEl, sumaIdx, moc);
         if (niebiore != -1)
         {
             return niebiore;
         }
+    }
+    if (moc < MOC) 
+    {
         int biore = func(t, idx + 1, sumaEl + t[idx], sumaIdx + idx, moc + 1);
         if (biore != -1)
-            {
-                return biore;
-            }
+        {
+            return biore;
+        }
     }
-    else 
-    {
-        return -1;
-    }
+
+    // jeżeli wcześniej nigdzie nie zwróciłem sukcesu (czyli sumaEl) no to zwracam -1
+    return -1;
+    
 
 }
 
