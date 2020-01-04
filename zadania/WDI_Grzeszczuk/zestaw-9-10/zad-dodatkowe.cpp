aa5c6a43833aa8ce1435658ac67568542d4b083f
kkafar <50801299+kkafar@users.noreply.github.com>
2020-01-04 13:44:22 +0100

    Zadania dodatkowe 1-6

--- zadania/WDI_Grzeszczuk/zestaw-9-10/zad-dodatkowe.cpp
+++ zadania/WDI_Grzeszczuk/zestaw-9-10/zad-dodatkowe.cpp
@@ -12,6 +12,14 @@ listy.
 4. Dana jest niepusta lista reprezentująca liczbę naturalną. Kolejne
 elementy listy przechowują kolejne cyfry. Proszę napisać funkcję
 zwiększającą taką liczbę o 1.
+5. Liczby naturalne reprezentowane jak poprzednim zadaniu. Proszę napisać
+funkcję dodającą dwie takie liczby. W wyniku dodawania dwóch liczb powinna
+powstać nowa lista.
+6. Lista zawiera niepowtarzające się elementy. Proszę napisać funkcję do
+której przekazujemy wskaźnik na początek oraz wartość klucza. Jeżeli
+element o takim kluczu występuje w liście należy go usunąć z listy. Jeżeli
+elementu o zadanym kluczu brak w liście należy element o takim kluczu
+wstawić do listy.
 */
 
 #include <iostream>
@@ -35,6 +43,27 @@ void push_front(node * &first, const int& value)
     tmp->next = first;
     first = tmp;
 }
+
+void delete_list(node * &first)
+{
+    node * tmp;
+    for (register node * iter = first; iter != NULL;)
+    {
+        tmp = iter;
+        iter = iter->next;
+        delete tmp;
+    }
+    first = NULL;
+}
+
+void print_list(node * first)
+{
+    for (; first != NULL; first = first->next)
+        cout << first->val << " ";
+    
+    cout << "\n";
+}
+
 
 /// ==================================================
 /*  
@@ -199,25 +228,114 @@ void add_one(node * first)
 /// ==================================================
 
 
-void delete_list(node * &first)
+/// ==================================================
+/*
+    ZADANIE 5 DODATKOWE
+*/
+node * add_numbs(node * n1, node * n2)
 {
-    node * tmp;
-    for (register node * iter = first; iter != NULL;)
-    {
-        tmp = iter;
-        iter = iter->next;
-        delete tmp;
-    }
-    first = NULL;
-}
+    node * sum = NULL;
 
-void print_list(node * first)
-{
-    for (; first != NULL; first = first->next)
-        cout << first->val << " ";
+    /*
+        TO DO
+        Co gdy jedna albo obie listy są puste? 
+        Trzeba to jakoś "oifować"
+    */
+
+   // tak jak w poprzednim zadaniu: reszta
+   int overflow = 0;
+
+   // dopóki jedna z liczb się "nie wyczerpie" przeprowadzamy normalne dodawanie pisemne 
+   while (n1 != NULL && n2 != NULL)
+   {
+       // użycie push_back spowodowałoby że nasza liczba byłaby od razu "w dobrą stronę", ale wtedy mielibyśmy od 
+       // razu O(n^2), a po co, jeżeli nie trzeba? Na końcu liniowo odwrócimy sobie liczbę. 
+       push_front(sum, n1->val + n2->val + overflow);
+       overflow = sum->val / 10;
+       sum->val %= 10;
+       n1 = n1->next;
+       n2 = n2->next;
+   }
+
+   // jeżeli któraś liczba się wyczerpała 
+    while (n1 != NULL)
+    {
+        push_front(sum, n1->val + overflow);
+        overflow = sum->val / 10;
+        sum->val %= 10;
+        n1 = n1->next;
+    }
+
+    while (n2 != NULL)
+    {
+        push_front(sum, n2->val + overflow);
+        overflow = sum->val / 10;
+        sum->val %= 10;
+        n2 = n2->next;
+    }
     
-    cout << "\n";
+    // jeszcze jest możliwość, że po dodaniu obydwu list overflow > 0
+    while (overflow != 0)
+    {
+        push_front(sum, overflow % 10);
+        overflow /= 10;
+    }
+
+    // na końcu trzeba jeszcze obrócić listę sum
+    node * queue = NULL;
+    for (register node * iter = sum; iter != NULL; iter = iter->next)
+        push_front(queue, iter->val);
+    
+    for (register node * iter1 = sum, * iter2 = queue; iter1 != NULL; iter1 = iter1->next, iter2 = iter2->next)
+        iter1->val = iter2->val;
+    
+    delete_list(queue);
+    
+    return sum;
 }
+/// ==================================================
+
+/// ==================================================
+/*
+    ZADANIE 6 DODATKOWE
+*/
+void del_by_key(node * & list, int key_val)
+{
+    if (list == NULL)
+    {
+        push_front(list, key_val);
+        return;
+    }
+
+    // special case dla pierwszego elementu listy
+    if (list->val == key_val)
+    {
+        node * tmp = list;
+        list = list->next;
+        delete tmp;
+        return;
+    }
+    // zaczynamy przeszukiwanie od drugiego elementu, jeżeli go nie ma to nic się nie dzieje
+    for (register node * iter = list->next, * sec_last = list; iter != NULL;)
+    {
+        // jeżeli znajdziemy key_val, to usuwamy ten element i kończymy działanie procedury
+        if (iter->val == key_val)
+        {
+            sec_last->next = iter->next;
+            delete iter;
+            return;
+        }
+        sec_last = iter;
+        iter = iter->next;
+    }
+
+    // jeżeli jesteśmy w tym miejscu, to wiemy że wartość nie została znaleziona ==> dodajemy ją na początek listy
+    push_front(list, key_val);
+    
+    return;
+}
+/// ==================================================
+
 
 int main()
 {
@@ -293,6 +411,45 @@ int main()
     print_list(list); print_list(list2);
     add_one(list); add_one(list2);
     print_list(list); print_list(list2);
+*/
+
+    // Zadanie 5 (dodatkowe) 
+/*
+    // Wygląda na to, że działa. Pamiętajmy że wynik jest odwrócony! ( cyfra jedności jest pierwsza )
+    node * list = NULL, * n1 = NULL, * n2 = NULL, * sum = NULL;
+
+    for (int i = 1; i < 4; ++i)
+    {
+        push_front(n1, i);
+        push_front(n2, 2*i);
+    }
+    print_list(n1); print_list(n2);
+    sum = add_numbs(n1, n2);
+    print_list(sum);
+    delete_list(n1); delete_list(n2); delete_list(sum);
+    push_front(n1, 9); push_front(n1, 9); push_front(n1, 9);
+    push_front(n2, 2); push_front(n2, 1);
+    print_list(n1); print_list(n2);
+    sum = add_numbs(n1, n2);
+    print_list(sum);
+    delete_list(n1); delete_list(n2); delete_list(sum);
+*/
+
+    // Zadanie 6 (dodatkowe)
+/*
+    node * list = NULL;
+
+    for (int i = 0; i < 5; ++i)
+        push_front(list, i);
+
+    print_list(list);
+    del_by_key(list, 2);
+    print_list(list);
+    del_by_key(list, 0);
+    print_list(list);
+    del_by_key(list, 4);
+    print_list(list);
+    delete_list(list);
 */
     return 0;
 }
