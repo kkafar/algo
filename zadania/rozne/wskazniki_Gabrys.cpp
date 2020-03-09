50911377fcd1eedce29ffccb9ad9e631507f0732
Kacper Kafara <50801299+kkafar@users.noreply.github.com>
2020-03-09 18:00:52 +0100

    09.03.2020 - pomoc dydaktyczna :O

---
+++ zadania/rozne/wskazniki_Gabrys.cpp
@@ -0,0 +1,59 @@
+/*
+	Quicksort!  
+*/
+#include <iostream>
+#include <time.h>
+
+#include "/home/kkafara/workspace/include/my-utility.h"
+using namespace std;
+
+const int SIZE = 9;
+
+void sumuj_wiersze(int ** arr, int size)
+{
+	// TO DO
+	return;
+}
+
+void write_tab(int ** arr, int col, int row)
+{
+	for (int i = 0; i < row; ++i)
+	{
+		for (int j = 0; j < col; ++j)
+			cout <<  &arr[i][j] << " " << arr[i][j] << " ";
+		cout << "\n";
+	}
+}
+
+
+int main()
+{
+	int ** arr = new int * [SIZE];
+
+	for (int i = 0; i < SIZE; ++i)
+		arr[i] = new int [SIZE];
+
+	for (int i = 0; i < SIZE; ++i)
+	{
+		for (int j = 0; j < SIZE; ++j)
+			arr[i][j] = i + j;
+	}
+/*
+	for (int * i = arr[0]; i < arr[0] + SIZE * SIZE; ++i)
+	{
+		cout << i << "  " << *i << "\n";
+	}
+*/
+	write_tab(arr, SIZE, SIZE);
+
+	cout << "\n\n" << &arr << "  <-- adres pod ktorym znajduje sie wskaznik na tablice 2d\n";
+	cout << arr << "  <-- adres na ktory wskazuje wskaznik na tablice 2d\n";
+	cout << *arr << "  <-- to powinien być adres elementu arr[0][0]\n\n";
+
+	for (int i = 0; i < SIZE; ++i)
+		cout << arr[i] << "  <-- adres pod ktorym znajduje sie 1. el. wiersza " << i << "\n" 
+		<< (arr + i) << "  <-- adres pod ktorym znajduje sie wskaznik na 1. el. wiersza " << i << "\n" 
+		<< &arr[i] << "  <-- adres pod ktorym znajduje sie wskaznik na 1. el. wiersza " << i << "\n\n";
+	
+	return 0; 
+}
